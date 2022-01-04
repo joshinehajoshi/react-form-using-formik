d6f1816415a739f2abd0d228ff21f97541c6f612
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-04 15:35:09 +0530

    yup added

--- src/Component/SignUp.js
+++ src/Component/SignUp.js
@@ -18,21 +18,42 @@ function SignUp() {
                 <h1 className="my-4 font-weight-bold-display-4">Sign Up</h1>
                 {console.log(formik)}
                 <Form>
-                    <TextField label="Fisrt Name" name="firstName"
+                    <TextField 
+                    label="Fisrt Name" 
+                    name="firstName"
                     type="text"
                      />
-                      <TextField label="Last Name" name="lastName"
-                    type="text"
+                      <TextField 
+                      label="Last Name" 
+                      name="lastName"
+                      type="text"
                      />
-                      <TextField label="Email" name="email"
-                    type="email"
+                      <TextField 
+                      label="Email" 
+                      name="email"
+                      type="email"
                      />
-                      <TextField label="Password" name="password"
-                    type="password"
+                      <TextField 
+                      label="Password" 
+                      name="password"
+                      type="password"
                      />
-                      <TextField label="Confirm Password" name="confirmPassword"
-                    type="password"
+                      <TextField 
+                      label="Confirm Password"      name="confirmPassword"
+                      type="password"
                      />
+                     <button
+                     className="btn btn-dark mt-3"
+                     type="submit"
+                     >
+                     Register
+                     </button>
+                     <button
+                     className="btn btn-danger mt-3 ml-3"
+                     type="reset"
+                     >
+                     Reset
+                     </button>
                 </Form>
             </div>
         )
