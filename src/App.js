f5443bf565d3a5c37aa788676cb6f329a625deac
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-04 12:03:21 +0530

    img added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Container } from 'react-bootstrap';
 import './App.css';
+import signUpImage  from './images/signup.jpg'
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello React</h1>
-    </div>
+   <div className="container mt-3">
+     <div classNam="row">
+        <div className="col-md-5">
+          SignUp
+        </div>
+        <div className="col-md-7">
+          <img src={signUpImage}></img>
+        </div>
+     </div>
+
+   </div>
   );
 }
 
