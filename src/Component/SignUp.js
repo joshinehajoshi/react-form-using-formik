8e8b7d1616ca131ead8d819282c678abb5a4eaa1
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-04 14:57:05 +0530

    form fields added

--- src/Component/SignUp.js
+++ src/Component/SignUp.js
@@ -21,6 +21,18 @@ function SignUp() {
                     <TextField label="Fisrt Name" name="firstName"
                     type="text"
                      />
+                      <TextField label="Last Name" name="lastName"
+                    type="text"
+                     />
+                      <TextField label="Email" name="email"
+                    type="email"
+                     />
+                      <TextField label="Password" name="password"
+                    type="password"
+                     />
+                      <TextField label="Confirm Password" name="confirmPassword"
+                    type="password"
+                     />
                 </Form>
             </div>
         )
--- src/Component/TextField.js
+++ src/Component/TextField.js
@@ -5,6 +5,7 @@ function TestField({label, ...props}) {
     const [field, meta] =  useField(props);
     return (
         <div className="mb-2">
+            <label htmlFor={field.name}>{label}</label> <br />
             <input type="text" />
         </div>
     )
